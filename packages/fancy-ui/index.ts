e43ded0644568a9d2d41a3a68c49cd45abcf5b4a
Benjamin Clos <benjamin.clos@gmail.com>
2024-03-18 15:13:05 -0600

    chore: prune demo package;

--- packages/fancy-ui/index.ts
+++
@@ -1,8 +0,0 @@
-export class FancyUi extends HTMLElement {
-  constructor () {
-    super()
-  }
-}
-
-window.customElements.define("fancy-ui", FancyUi)
-flowplayer.customElements.set("flowplayer-ui", "fancy-ui")

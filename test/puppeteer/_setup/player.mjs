602f3bf38e961d755c3129bc991649d03fc3474e
alexarassat <alexandros.satanakis@gmail.com>
2024-09-26 15:44:28 +0300

    chore: add player test method

--- test/puppeteer/_setup/player.mjs
+++ test/puppeteer/_setup/player.mjs
@@ -1,60 +1,44 @@
 export async function captureEvent(page, player, eventName, timeout = 20_000) {
-  return await page.evaluate(
-    (player, eventName, timeout) => {
-      return new Promise((ok, err) => {
-        const serialize = (o) => {
-          try {
-            return JSON.parse(JSON.stringify(o))
-          } catch (err) {
-            return o
-          }
-        }
+    return await page.evaluate(
+        (player, eventName, timeout) => {
+            return new Promise((ok, err) => {
+                const serialize = (o) => {
+                    try {
+                        return JSON.parse(JSON.stringify(o))
+                    } catch (err) {
+                        return o
+                    }
+                }
 
-        const handle = (e) => {
-          ok({
-            type: e.type,
-            state: player.root.className,
-            detail: serialize(e.detail)
-          })
-        }
-        player.addEventListener(eventName, handle, { once: true })
-        setTimeout(
-          () =>
-            err(
-              new Error(`failed to detect event::${eventName} in ${timeout}ms`)
-            ),
-          timeout
-        )
-      })
-    },
-    player,
-    eventName,
-    timeout
-  )
+                const handle = (e) => {
+                    ok({
+                        type: e.type,
+                        state: player.root.className,
+                        detail: serialize(e.detail)
+                    })
+                }
+                player.addEventListener(eventName, handle, {once: true})
+                setTimeout(
+                    () =>
+                        err(
+                            new Error(`failed to detect event::${eventName} in ${timeout}ms`)
+                        ),
+                    timeout
+                )
+            })
+        },
+        player,
+        eventName,
+        timeout
+    )
 }
 
-export async function seek(page, player, time) {
-  return await page.evaluate(
-    (player, time) => {
-      return new Promise((ok, err) => {
-        const handle = (e) =>
-          ok({ type: e.type, state: player.root.className, detail: e.detail })
-        player.addEventListener("timeupdate", handle, { once: true })
-        setTimeout(() => err(new Error(`seeking operation failed`)), 20000)
-        player.currentTime = time
-      })
-    },
-    player,
-    time
-  )
-}
-
-export async function toggleMute(page, player, flag) {
-  return await page.evaluate(
-    (player, flag) => {
-      return Promise.resolve(player.toggleMute(flag))
-    },
-    player,
-    flag
-  )
+export async function togglePlay(page, player, flag) {
+    return await page.evaluate(
+        (player, flag) => {
+            return Promise.resolve(player.togglePlay(flag))
+        },
+        player,
+        flag
+    )
 }
